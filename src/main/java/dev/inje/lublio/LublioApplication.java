683d41f5ae5fec62b2a45d19d49a27c2a00b1c4d
Luan Pak <grandefidelite@gmail.com>
2024-06-06 20:04:23 +0900

    First Commit from IntelliJ

---
+++ src/main/java/dev/inje/lublio/LublioApplication.java
@@ -0,0 +1,13 @@
+package dev.inje.lublio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LublioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LublioApplication.class, args);
+    }
+
+}
